27d6174ef9c902fc4defe7ababdc58b1b93214d1
Egoriik <egor.bazylev07@gmail.com>
2026-03-11 10:18:06 +0300

    6.1-6.4 Решено

--- 6.2.py
+++
@@ -1,9 +0,0 @@
-def f(num):
-    if num == 0:
-        print("На ноль делить нельзя!")
-    else:
-        res = 100/num
-        print(f"100 : {num} = {res}")
-
-num = int(input("Введите число: "))
-f(num)
